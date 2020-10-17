3c16f41a9bba9ed5b07afe43c5f7081ee2f10106
kosugi2000 <69379415+kosugi2000@users.noreply.github.com>
2020-10-17 16:50:00 +0900

    Update post.php

--- kadai_php01_17_kosugi/post.php
+++ kadai_php01_17_kosugi/post.php
@@ -2,27 +2,21 @@
 
 <head>
     <meta charset="utf-8">
-    <style>
-        .menu {
-            background-color: #2FA6E9;
-        }
-    </style>
-    <title>POST練習</title>
+    <title>課題テンプレート</title>
 </head>
 
 <body>
-    <div class="menu">
-        <h3>menu</h3>
-        <ul>
-            <li>サンプルフォームに必要な情報を入れる(POST)</li>
-            <li>(演習)password欄を作って受け取る。</li>
-            <li><a href="index.php">index.php</a></li>
-        </ul>
-    </div>
-    <form action=" " method=" ">
-        お名前: <input type="text" name=" ">
-        EMAIL: <input type="text" name=" ">
-        <!-- パスワード欄を追加してみる。 -->
+    <form action="write.php" method="post">
+        　お名前: <input type="text" name="name"> 
+        　EMAIL: <input type="text" name="mail">
+        　年齢：<input type="text" name="age">
+        　電話番号：<input type="text" name="tel">
+        　満足度：<select name="exp">
+         <option value="無回答"></option>
+         <option value="期待以上">期待以上</option>
+         <option value="普通">普通</option>
+         <option value="要改善">要改善</option>
+         </select>
         <input type="submit" value="送信">
     </form>
 </body>
