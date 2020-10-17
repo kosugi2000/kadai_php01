fcf9ca130c9a382928c358acbd7c931bb0e4a05f
kosugi2000 <69379415+kosugi2000@users.noreply.github.com>
2020-10-17 13:34:26 +0900

    Update read.php

--- kadai_php01_17_kosugi/read.php
+++ kadai_php01_17_kosugi/read.php
@@ -1,6 +1,50 @@
 <?php
-// ファイルを開く
+$openFile = fopen('./data/data.csv','r');
 
-// ファイル内容を1行ずつ読み込んで出力
+function h($str){
+    return htmlspecialchars($str, ENT_QUOTES);
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>集計</title>
+</head>
+<body>
 
-// ファイルを閉じる
+
+
+<table border="1">
+        <tr>
+            <th>回答日時</th>
+            <th>お名前</th>
+            <th>EMAIL</th> 
+            <th>年齢</th>
+            <th>電話番号</th>
+            <th>満足度</th>
+        </tr>
+        
+        <?php 
+   
+        while($str = fgets($openFile)){
+            $ary = explode("," , $str);
+            
+            ?>
+            <tr>
+                <td><?=h($ary[0])?></td>
+                <td><?=h($ary[1])?></td>
+                <td><?=h($ary[2])?></td>
+                <td><?=h($ary[3])?></td>
+                <td><?=h($ary[4])?></td>
+                <td><?=h($ary[5])?></td>
+            </tr>
+            <?php
+ 
+
+            
+            }
+          
+            ?>
+    </table>
