46ad46269e660cfafdc57411ac689bac7e3a660d
kosugi2000 <69379415+kosugi2000@users.noreply.github.com>
2020-10-17 16:51:01 +0900

    Update write.php

--- kadai_php01_17_kosugi/write.php
+++ kadai_php01_17_kosugi/write.php
@@ -1,7 +1,18 @@
 <?php
 // ファイルに書き込み
+$time = date('Y-m-d H:i:s');
+$age = $_POST['age'];
+$mail = $_POST['mail'];
+$name = $_POST['name'];
+$tel = $_POST['tel'];
+$exp = $_POST['exp'];
 
+$str = $time. ',' .$name. ',' .$mail. ',' .$age. ',' .$tel. ',' .$exp;
 //文字作成
+
+$file = fopen('./data/data.csv','a');
+fwrite( $file, $str ."\n"); 
+fclose($file);
 
 ?>
 
@@ -16,10 +27,11 @@
 <body>
 
     <h1>書き込みしました。</h1>
-    <h2>./data/data.txt を確認しましょう！</h2>
-
+    <h2>./data/data.csv を確認しましょう！</h2>
+  <p><?= $str ?></p>
     <ul>
-        <li><a href="input.php">戻る</a></li>
+        <li><a href="read.php">データ集計をみる</a></li>
+        <li><a href="post.php">入力フォームに戻る</a></li>
     </ul>
 </body>
 
